24f6cab62e692f808d60f73c926dde841fff6ad8
Himindu <himindukularathne@gmail.com>
2023-12-14 02:47:30 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Himindu Kularathne on 2023-12-14.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
